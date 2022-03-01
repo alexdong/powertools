ed03b4cca34d3ef8f30d1a6048bc786c5757cd5d
Alex Dong <alex.dong@keepers.photos>
2022-03-02 06:54:49 +1300

    add `-h`

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# powertools
+`powertools` is a collection of small, single-purpose tools I built using [crystal-lang](https://crystal-lang.org/). To install the tools into `~/bin`, clone the repo then do `brew install crystal && ./build.sh`.
+
+### humantime
+`humantime` turns an epoch ms time into local timezone string. 
+
+```bash
+# Manually look up a time
+humantime 1646112277384
+
+# Or, be part of the pipe for log analysis
+echo 1646112277384 | humantime"
+tail -F ... | cut -f 3 | humantime
+```
--- humantime.cr
+++ humantime.cr
@@ -1,14 +1,34 @@
+require "option_parser"
+
+banner = "Convert an milli-second epoch into string in local timezone
+
+Usage: 
+
+    humantime 1646112277384
+    echo 1646112277384 | humantime
+"
+
+option_parser = OptionParser.parse do |parser|
+  parser.banner = banner
+  parser.on "-v", "--version", "Show version" do
+    puts "version 1.0.0"
+    exit
+  end
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+end
+
+
 epoch_ms = ARGV[0] if ARGV.size > 0
-epoch_ms = STDIN.gets_to_end if ARGV.size == 0
+epoch_ms = STDIN.gets if ARGV.size == 0
 
 case epoch_ms
-when Nil
-    puts "Convert an milli-second epoch into string in local timezone
-
-    Usage: 
-
-        humantime 1646112277384
-        echo 1646112277384 | humantime"
+when Nil, ""
+    STDERR.puts "ERROR: missing timestamp"
+    STDERR.puts option_parser
+    exit(1)
 else
     puts Time.unix_ms(epoch_ms.to_i64).to_local.to_s("%Y-%m-%d %H:%M:%S.%L %z")
 end
